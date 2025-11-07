570cc59557f414024fb3926b6d7dc96407e420f1
Alex Holmes <alex@sed.dev>
2025-11-07 13:42:49 -0500

    Add sqlparser dep

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@ use std::fmt::Debug;
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use futures::{stream, Sink, SinkExt};
+use futures::{stream, Sink};
 use pgwire::api::{ClientInfo, ClientPortalStore, NoopHandler, PgWireServerHandlers, Type};
 use pgwire::api::query::SimpleQueryHandler;
 use pgwire::api::results::{DataRowEncoder, FieldFormat, FieldInfo, QueryResponse, Response, Tag};
 use pgwire::error::{ErrorInfo, PgWireError, PgWireResult};
 use pgwire::messages::PgWireBackendMessage;
 use pgwire::tokio::process_socket;
+use sqlparser::ast::Statement;
+use sqlparser::dialect::PostgreSqlDialect;
+use sqlparser::parser::Parser;
 use tokio::net::TcpListener;
 
 pub struct Config {
@@ -36,32 +39,110 @@ where
     PgWireError: From<<C as Sink<PgWireBackendMessage>>::Error>,
 {
     println!("Received query: {:?}", query);
-    let resp = match query {
-        "BEGIN;" => Response::TransactionStart(Tag::new("BEGIN")),
-        "ROLLBACK;" => Response::TransactionEnd(Tag::new("ROLLBACK")),
-        "COMMIT;" => Response::TransactionEnd(Tag::new("COMMIT")),
-        "SELECT 1;" => {
-            let f1 =
-                FieldInfo::new("?column?".into(), None, None, Type::INT4, FieldFormat::Text);
-            let schema = Arc::new(vec![f1]);
-            let schema_ref = schema.clone();
 
-            let mut encoder = DataRowEncoder::new(schema_ref.clone());
-            encoder.encode_field(&1i32)?;
-            let row = encoder.finish();
+    let dialect = PostgreSqlDialect {};
+    let ast = Parser::parse_sql(&dialect, query).map_err(|e| {
+        PgWireError::UserError(Box::new(ErrorInfo::new(
+            "ERROR".to_string(),
+            "42601".to_string(), // syntax_error
+            format!("Parse error: {}", e),
+        )))
+    })?;
 
-            let data_row_stream = stream::iter(vec![row]);
+    if ast.is_empty() {
+        return Ok(vec![Response::EmptyQuery]);
+    }
 
-            Response::Query(QueryResponse::new(schema, data_row_stream))
-        }
-        _ => Response::Error(Box::new(ErrorInfo::new(
-            "FATAL".to_string(),
-            "38003".to_string(),
-            "Unsupported statement.".to_string(),
-        ))),
-    };
+    let mut responses = Vec::new();
+    for statement in ast {
+        let resp = match statement {
+            Statement::StartTransaction { .. } => {
+                Response::TransactionStart(Tag::new("BEGIN"))
+            }
+            Statement::Rollback { .. } => {
+                Response::TransactionEnd(Tag::new("ROLLBACK"))
+            }
+            Statement::Commit { .. } => {
+                Response::TransactionEnd(Tag::new("COMMIT"))
+            }
+            Statement::Query(query) => {
+                // Check if it's "SELECT 1"
+                if let sqlparser::ast::SetExpr::Select(select) = &*query.body {
+                    if select.projection.len() == 1 {
+                        if let sqlparser::ast::SelectItem::UnnamedExpr(expr) = &select.projection[0] {
+                            if let sqlparser::ast::Expr::Value(val) = expr {
+                                if let sqlparser::ast::Value::Number(n, _) = &val.value {
+                                    if n == "1" && select.from.is_empty() {
+                                        // Handle SELECT 1
+                                        let f1 = FieldInfo::new(
+                                            "?column?".into(),
+                                            None,
+                                            None,
+                                            Type::INT4,
+                                            FieldFormat::Text,
+                                        );
+                                        let schema = Arc::new(vec![f1]);
+                                        let schema_ref = schema.clone();
 
-    Ok(vec![resp])
+                                        let mut encoder = DataRowEncoder::new(schema_ref.clone());
+                                        encoder.encode_field(&1i32)?;
+                                        let row = encoder.finish();
+
+                                        let data_row_stream = stream::iter(vec![row]);
+                                        Response::Query(QueryResponse::new(schema, data_row_stream))
+                                    } else {
+                                        Response::Error(Box::new(ErrorInfo::new(
+                                            "ERROR".to_string(),
+                                            "0A000".to_string(), // feature_not_supported
+                                            "Only SELECT 1 is supported".to_string(),
+                                        )))
+                                    }
+                                } else {
+                                    Response::Error(Box::new(ErrorInfo::new(
+                                        "ERROR".to_string(),
+                                        "0A000".to_string(),
+                                        "Only SELECT 1 is supported".to_string(),
+                                    )))
+                                }
+                            } else {
+                                Response::Error(Box::new(ErrorInfo::new(
+                                    "ERROR".to_string(),
+                                    "0A000".to_string(),
+                                    "Only SELECT 1 is supported".to_string(),
+                                )))
+                            }
+                        } else {
+                            Response::Error(Box::new(ErrorInfo::new(
+                                "ERROR".to_string(),
+                                "0A000".to_string(),
+                                "Only SELECT 1 is supported".to_string(),
+                            )))
+                        }
+                    } else {
+                        Response::Error(Box::new(ErrorInfo::new(
+                            "ERROR".to_string(),
+                            "0A000".to_string(),
+                            "Only SELECT 1 is supported".to_string(),
+                        )))
+                    }
+                } else {
+                    Response::Error(Box::new(ErrorInfo::new(
+                        "ERROR".to_string(),
+                        "0A000".to_string(),
+                        "Unsupported query type".to_string(),
+                    )))
+                }
+            }
+            _ => Response::Error(Box::new(ErrorInfo::new(
+                "ERROR".to_string(),
+                "0A000".to_string(),
+                format!("Unsupported statement: {:?}", statement),
+            ))),
+        };
+        responses.push(resp);
+    }
+
+    Ok(responses)
 }
 }
 
